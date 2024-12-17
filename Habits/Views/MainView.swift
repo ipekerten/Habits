18b20d8d127b79b09465f132b9b5b28254a54517
ipekerten <ipek.erten@ozu.edu.tr>
2024-12-17 20:56:36 +0100

    added app icon

--- Habits/Views/MainView.swift
+++ Habits/Views/MainView.swift
@@ -61,6 +61,9 @@ struct MainView: View {
                                 }
                             }
                         }
+                        
+                       
+                        
                     } label: {
                         Image(isHabitLogged ? "ButtonDone" : "Button")
                             .resizable()
